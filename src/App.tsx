4828f51ed64e73404a77ac79b2a56f60609ad853
Gabriella Borges <gabimullerborges@gmail.com>
2023-06-27 19:17:38 -0300

    feat: iniciada a classe livros

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Botao from './components/botao';
+
 
 function App() {
+  const [livros, setLivros] = useState([
+    {
+    id: 1,
+    title: "Harry Potter",
+    author: "J K Rollin",
+    category: "Ficção",
+    recomendation: 0
+  }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hint Library</h1>
+      <div className='lista-livros'>
+        {livros.map((livro)=> (
+          <div className='livro'>
+            <div className='content'>
+              <p>{livro.title}</p>
+              <p>{livro.author}</p>
+              <p>{livro.category}</p>
+              <p>{livro.recomendation}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Botao/>
     </div>
   );
 }
